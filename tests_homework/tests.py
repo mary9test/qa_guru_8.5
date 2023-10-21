59f38d196764be4ae69d19ac4df9c45b3bbf729c
maria.m <maryqatest9@gmail.com>
2023-10-21 20:45:27 +0300

    Реализована вторая часть

--- tests_homework/tests.py
+++ tests_homework/tests.py
@@ -1,38 +1,26 @@
-import os.path
-from selene import browser, have
-
-def test_fill__registration_form():
-    browser.open("/automation-practice-form")
-    browser.element('#firstName').type('Иван')
-    browser.element('#lastName').type('Петров')
-    browser.element('#userEmail').type('petrov@gmail.com')
-    browser.element("[for='gender-radio-1']").click()
-    browser.element('#userNumber').type('9001010800')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('[class="react-datepicker__month-select"]').click().element('[value="0"]').click()
-    browser.element('[class="react-datepicker__year-select"]').click().element('[value="1998"]').click()
-    browser.element('[class="react-datepicker__day react-datepicker__day--008"]').click()
-    browser.element('#subjectsInput').type('Chemistry').press_enter()
-    browser.element("[for='hobbies-checkbox-1']").click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('img/img.png'))
-    browser.element('#currentAddress').type('12 street')
-    browser.element('#state').click()
-    browser.element('#react-select-3-input').type('Haryana').press_enter()
-    browser.element('#city').click()
-    browser.element('#react-select-4-input').type('Karnal').press_enter()
-    browser.element('#submit').click()
-
-    browser.element('.table-responsive').all('td:nth-of-type(2)').should(have.texts(
-        'Иван Петров',
-        'petrov@gmail.com',
-        'Male',
-        '9001010800',
-        '08 January,1998',
-        'Chemistry',
-        'Sports',
-        'img.png',
-        '12 street',
-        'Haryana Karnal'))
+from pages.registration_form import RegistrationPage
+from tests_homework.users import User
 
 
+def test_fill_registration_form():
+    registration_page = RegistrationPage()
+    user = User(
+        last_name='Петров',
+        first_name='Иван',
+        email='petrov@gmail.com',
+        gender='Male',
+        phone='9001010800',
+        day='08',
+        month='0',
+        year='1998',
+        subject='Chemistry',
+        hobby='Sports',
+        picture='img.png',
+        address='12 street',
+        state='Haryana',
+        city='Karnal'
 
+    )
+    registration_page.open()
+    registration_page.register_user(user)
+    registration_page.check_registration(user)
