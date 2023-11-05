c50c9e3ae6d9aae7d1131c447ca555ebdf7a1160
maria.m <maryqatest9@gmail.com>
2023-11-05 15:06:23 +0300

    12 homework

--- test/conftest.py
+++ test/conftest.py
@@ -24,7 +24,8 @@ def setup_browser(request):
 
     browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.driver.maximize_window()
+    browser.config.window_width = 2880
+    browser.config.window_height = 1800
 
     yield browser
 
