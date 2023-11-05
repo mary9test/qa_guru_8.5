226529960e8c918115c6922bfa426c906dee0421
maria.m <maryqatest9@gmail.com>
2023-11-05 20:20:45 +0300

    14 homework

--- test/conftest.py
+++ test/conftest.py
@@ -1,57 +1,29 @@
-import os
-
 import pytest
-from dotenv import load_dotenv
-
-from selene import browser
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 from utils import attach
-
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def set_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-    site = os.getenv('SITE')
-
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@{site}",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
     browser.config.driver = driver
-
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 20
     browser.config.window_width = 2880
     browser.config.window_height = 1800
 
