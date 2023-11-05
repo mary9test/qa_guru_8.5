9bb3ce9f214bfe5a21372ec94ac15fa384ac2f2e
maria.m <maryqatest9@gmail.com>
2023-11-05 20:34:41 +0300

    14 homework

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,26 @@
+import os
+
 import pytest
-from selene.support.shared import browser
+from dotenv import load_dotenv
+
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
 from utils import attach
 
 
+def pytest_addoption(parser):
+    parser.addoption('--remote_url', default='')
+
+
 @pytest.fixture(scope='function', autouse=True)
-def setup_browser(request):
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def init_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -17,12 +31,16 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    remote_url = request.config.getoption('--remote_url')
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{remote_url}",
         options=options
     )
 
     browser.config.driver = driver
+
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 2880
     browser.config.window_height = 1800
